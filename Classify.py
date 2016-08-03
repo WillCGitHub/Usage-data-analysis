e121735f72675f6638247164c7ddc0d0d8bb4b0c
WillCGitHub <williamc.c999@gmail.com>
2016-08-03 13:15:34 +0200

    use new categories

--- Classify.py
+++ Classify.py
@@ -7,6 +7,7 @@ import sys
 import csv
 import string
 from collections import Counter
+import pandas as pd
 
 
 
@@ -18,6 +19,53 @@ def save_obj(obj, name):
     with open('userdb/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
+category_dict = {'100':'No Profile',
+					'101':'Trade Partner',
+					'102':'Think Tank',
+					'103':'Consortia',
+					'104':'Industry',
+					'105':'Parliament',
+					'106':'End Client',
+					'107':'Government',
+					'108':'International Organisation',
+					'109':'Library',
+					'110':'Academic',
+					'111':'Dataseller',
+					'112':'NGOs & Civil Soc Org & Trade Uni',
+					'114':'Financial & Legal Services'}
+cu_profile = 'TurpinFiles/xpslite_customer_profile.csv'
+df = pd.read_csv(cu_profile,header = 0,dtype = str)
+cu_profile_code_dict = dict()
+for row in df.itertuples():
+	if category_dict.get(row[3]) is not None:
+		cu_profile_code_dict[row[1].lower()] = row[3]
+print(len(cu_profile_code_dict))
+
+print('load userdb')
+user_dict = load_obj('user_dict')
+
+for u,d in user_dict.items():
+	cu = d.identity_dict.get('CU_number')
+	profile_code = cu_profile_code_dict.get(cu)
+	d.identity_dict['category'] = profile_code
+
+
+save_obj(user_dict,'user_dict')
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" OLD VERSION """
+"""
 def classify_category(name):
 	classify_dict = dict()
 	university = ["university","universidad","college","universidade","school",
@@ -114,12 +162,12 @@ id_info = dict()
 
 for idx, row in enumerate(ws.rows):
 	if id_info.get(row[1].value) is None:
-		"""
-		0 - name
-		3 - e-mail
-		9 - country
-		10 - CU number 
-		"""
+		
+		#0 - name
+		#3 - e-mail
+		#9 - country
+		#10 - CU number 
+		
 		id_info[row[1].value] = (row[0].value,
 								row[3].value,
 								row[9].value,
@@ -149,7 +197,7 @@ with open("category_result.csv", 'w') as f:
 	for a in r:
 		writer.writerow([a[0],a[1]])
 
-
+"""
 
 
 
